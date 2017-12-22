05adb0a91aeb94ccd6327a9d958382a224042acc
Dadan Satria <dadansatria7@gmail.com>
2017-12-22 21:13:44 +0700

    login form

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+
+	<center>
+	LOGIN FORM
+	<hr>
+		<table cellpadding="5">
+			<tr>
+				<td>Username</td>
+				<td>:</td>
+				<td><input type="" name=""></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td>:</td>
+				<td><input type="" name=""></td>
+			</tr>		
+		</table>
+	</center>
+</body>
+</html>
