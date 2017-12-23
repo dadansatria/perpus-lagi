c95d31043f74de7adcfcc8840597bb5330c61f3c
Dadan Satria <dadansatria7@gmail.com>
2017-12-23 11:24:16 +0700

    tambah.php

---
+++ tambah.php
@@ -0,0 +1,23 @@
+<html>
+<form>
+	<table>
+		<tr>
+			<td> Judul Buku </td>
+			<td> : </td>
+			<td> <input type="" name=""> </td>
+		</tr> 
+
+		<tr>
+			<td> Penulis </td>
+			<td> : </td>
+			<td> <input type="" name=""> </td>
+		</tr> 
+
+		<tr>
+			<td> Penerbit </td>
+			<td> : </td>
+			<td> <input type="" name=""> </td>
+		</tr> 
+	</table>
+</form>
+</html>
