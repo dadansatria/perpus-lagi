ffab39f8b9369f19be7ca56015d292997f3feec6
dheanatasilvia <dheasilvia74@gmail.com>
2017-12-23 10:57:47 -0800

    menambhakan tabel

---
+++ tabel.php
@@ -0,0 +1,46 @@
+
+
+<html>
+<head>
+   <title></title>
+</head>
+<body>
+<h1 align="center">DATA BUKU PERPUSTAKAAN</h1>
+<table border="1">
+    <tr>
+        <td>No</td>
+        <td>Judul Buku</td>
+        <td>Penulis</td>
+        <td>Tahun Terbit</td>
+        <td>Penerbit</td>
+    </tr>
+
+        <tr>
+        <td>1</td>
+        <td>Algoritma</td>
+        <td>Dheanata</td>
+        <td align="center">2011</td>
+        <td>Erlangga</td>
+    </tr>
+
+     <tr>
+        <td>3</td>
+        <td>Algoritma</td>
+        <td>Dheanata</td>
+        <td align="center">2011</td>
+        <td>Erlangga</td>
+    </tr>
+
+     <tr>
+        <td>3</td>
+        <td>Algoritma</td>
+        <td>Dheanata</td>
+        <td align="center">2011</td>
+        <td>Erlangga</td>
+    </tr>
+    
+</table> <br> 
+<a href="tambah.php"> Tambah </a> <br>
+<a href="logout.php"> Log Out </a>
+<body>
+</html>
