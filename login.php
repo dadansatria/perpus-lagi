18bb2980eecd2901eea965e7143a0952a566d58f
Dadan Satria <dadansatria7@gmail.com>
2017-12-23 12:43:06 +0700

    komentar

--- login.php
+++ login.php
@@ -4,22 +4,27 @@
 	<title>Login</title>
 </head>
 <body>
+	<form action="tabel.php">
+		<center>
+		LOGIN FORM
+		<hr>
+			<table cellpadding="5">
+				<tr>
+					<td>Username</td>
+					<td>:</td>
+					<td><input type="" name=""></td>
+				</tr>
+				<tr>
+					<td>Password</td>
+					<td>:</td>
+					<td><input type="" name=""></td>
+				</tr>	
+				<tr>
+					<td colspan="2"> <button>Login</button></td>
+				</tr>	
+			</table>
+		</center>
+	</form>
 
-	<center>
-	LOGIN FORM
-	<hr>
-		<table cellpadding="5">
-			<tr>
-				<td>Username</td>
-				<td>:</td>
-				<td><input type="" name=""></td>
-			</tr>
-			<tr>
-				<td>Password</td>
-				<td>:</td>
-				<td><input type="" name=""></td>
-			</tr>		
-		</table>
-	</center>
 </body>
 </html>
