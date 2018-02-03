c70c35f06bebc8ee55f347260821e3e174c9a875
dheanatasilvia <dheasilvia74@gmail.com>
2018-02-03 13:56:12 -0800

    mengupload login.php

---
+++ biodata.php
@@ -0,0 +1,149 @@
+<body bgcolor="pink">
+	<h2 align="center"> Form Biodata Anggota </h2>
+<form action="simpann.php" method="POST" />
+<table>
+	<tr>
+		<td> Nama Lengkap </td>
+		<td> : </td>
+		<td> <input type="text" name="nama" /> </td>
+	</tr>
+
+	<!-- <tr>
+		<td> User Name </td>
+		<td> : </td>
+		<td> <input type="text" name="userid" /> </td>
+	</tr> -->
+
+	<tr>
+		<td> Jurusan </td>
+		<td> : </td>
+		<td> <input type="radio"  name="jurusan" value="Teknik Informatika" /> Teknik Informatika <br> 
+			 <input type="radio"  name="jurusan" value="Teknik Sipil" /> Teknik Sipil<br>
+			 <input type="radio"  name="jurusan" value="Teknik Arsitektur" /> Teknik Arsitektur<br>
+			 <input type="radio"  name="jurusan" value="Teknik Elektronika" /> Teknik Elektronika </td>
+	</tr>
+	<tr>
+		<td> Bidang </td>
+		<td> : </td>
+		<td> <input type="radio"  name="bidang" value="Seni Peran" /> Seni Peran <br> 
+			 <input type="radio"  name="bidang" value="Seni Musik" /> Seni Musik<br>
+			 <input type="radio"  name="bidang" value="Seni Tari" /> Seni Tari<br>
+			 <input type="radio"  name="bidang" value=" Seni Tulis" /> Seni Tulis</td>
+	</tr>
+
+	
+
+	<tr>
+		<td> Email </td>
+		<td> : </td>
+		<td> <input type="text" name="email" /> </td>
+	</tr>
+	<tr>
+		<td> No Telepon </td>
+		<td> : </td>
+		<td> <input type="text" name="nohp" /> </td>
+	</tr>
+	<tr>
+		<td> Alamat </td>
+		<td> : </td>
+		<td><textarea cols="50" rows="5" name="alamat"></textarea><br/>
+
+	</tr>
+
+	
+
+	
+
+	<tr>
+		<td> <button type="submit" > Daftar</button>
+			 <button type="delete" > Batal</button>
+ 	</tr>
+</form>
+</table>
+</body>
+<!-- <tr>
+<td>Tanggal Lahir</td>
+
+<td> : <select name="tanggal">
+<option>01</option>
+<option>02</option>
+<option>03</option>
+<option>04</option>
+<option>05</option>
+<option>06</option>
+<option>07</option>
+
+<option>08</option>
+<option>09</option>
+<option>10</option>
+<option>11</option>
+<option>12</option>
+<option>13</option>
+<option>14</option>
+<option>15</option>
+<option>16</option>
+
+<option>17</option>
+<option>18</option>
+<option>19</option>
+<option>20</option>
+<option>21</option>
+<option>22</option>
+<option>23</option>
+<option>24</option>
+<option>25</option>
+
+<option>26</option>
+<option>27</option>
+<option>28</option>
+<option>29</option>
+<option>30</option>
+<option>31</option>
+</select>
+
+<select name="bulan">
+<option>Januari</option>
+
+<option>Februari</option>
+<option>Maret</option>
+<option>April</option>
+<option>Mei</option>
+<option>Juni</option>
+<option>Juli</option>
+<option>Agustus</option>
+<option>September</option>
+<option>Oktober</option>
+
+<option>November</option>
+<option>Desember</option>
+</select>
+
+<select name="tahun">
+<option>1980</option>
+<option>1981</option>
+<option>1982</option>
+<option>1983</option>
+<option>1984</option>
+
+<option>1985</option>
+<option>1986</option>
+<option>1987</option>
+<option>1988</option>
+<option>1989</option>
+<option>1990</option>
+<option>1991</option>
+<option>1992</option>
+<option>1993</option>
+
+<option>1994</option>
+<option>1995</option>
+<option>1996</option>
+<option>1997</option>
+<option>1998</option>
+<option>1999</option>
+<option>2000</option>
+</select>
+
+</td>
+
+</tr> -->
